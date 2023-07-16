0f88d62b0cb4b71e5d818761dce0461e2ed1bde8
Maurice-Pascal L <maurice.lariviere@icloud.com>
2023-07-16 04:31:45 +0200

    Rework of team page with component

---
+++ src/components/TeamContainer.vue
@@ -0,0 +1,66 @@
+<script lang="ts">
+
+import Instagram from '@/components/icons/Instagram.vue'
+import GitHub from '@/components/icons/GitHub.vue'
+import Twitter from '@/components/icons/Twitter.vue'
+import Spotify from '@/components/icons/Spotify.vue'
+import Twitch from '@/components/icons/Twitch.vue'
+
+export default {
+    props: {
+        name: String,
+        rank: String,
+        img: String,
+        ttv: String,
+        github: String,
+        twitter: String,
+        insta: String,
+        spotify: String
+    },
+    components: {
+        Instagram,
+        GitHub,
+        Twitter,
+        Spotify,
+        Twitch
+    },
+}
+
+</script>
+
+<template>
+    <div class="text-center text-gray-500 dark:text-gray-400">
+        <img class="mx-auto mb-4 w-36 h-36 rounded-full" alt="Bonnie Avatar" :src="img">
+        <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <a href="#">{{ name }}</a>
+        </h3>
+        <p>{{ rank }}</p>
+        <ul class="flex justify-center mt-4 space-x-4">
+            <li v-if="ttv != '' && ttv != null">
+                <a :href="ttv" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
+                    <Twitch></Twitch>
+                </a>
+            </li>
+            <li v-if="github != '' && github != null">
+                <a :href="github" target="_blank" class="text-gray-900 hover:text-gray-900 dark:hover:text-white dark:text-gray-300">
+                    <GitHub></GitHub>
+                </a>
+            </li>
+            <li v-if="twitter != '' && twitter != null">
+                <a :href="twitter" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
+                    <Twitter></Twitter>
+                </a>
+            </li>
+            <li v-if="insta != '' && insta != null">
+                <a :href="insta" target="_blank" class="text-gray-900 hover:text-gray-900 dark:hover:text-white dark:text-gray-300">
+                    <Instagram></Instagram>
+                </a>
+            </li>
+            <li v-if="spotify != '' && spotify != null">
+                <a :href="spotify" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
+                    <Spotify></Spotify>
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,21 +1,14 @@
 <script lang="ts">
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
-import Instagram from '@/components/Icons/Instagram.vue'
-import GitHub from '@/components/Icons/GitHub.vue'
-import Twitter from '@/components/Icons/Twitter.vue'
-import Spotify from '@/components/Icons/Spotify.vue'
-import Twitch from '@/components/Icons/Twitch.vue'
+
+import TeamContainer from '@/components/TeamContainer.vue'
 
 export default {
     components: {
         Navbar,
         Footer,
-        Instagram,
-        GitHub,
-        Twitter,
-        Spotify,
-        Twitch
+        TeamContainer
     }
 }
 
@@ -34,165 +27,58 @@ console.log('%c Wenn du nicht 100% weißt, was du tust, schließe dieses Fenster
                 <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400"></p>
             </div> 
             <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                <div class="text-center text-gray-500 dark:text-gray-400">
-                    <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://cdn.discordapp.com/avatars/234765355178917901/6706d060efe5caf93ebb770371a713ab.webp?size=128" alt="Bonnie Avatar">
-                    <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        <a href="#">KikiYT</a>
-                    </h3>
-                    <p>Creator / Inhaber</p>
-                    <ul class="flex justify-center mt-4 space-x-4">
-                        <li>
-                            <a href="https://twitch.tv/kikiyt90" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Twitch></Twitch>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/KikiYT90" target="_blank" class="text-gray-900 hover:text-gray-900 dark:hover:text-white dark:text-gray-300">
-                                <GitHub></GitHub>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/KikiYT90" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Twitter></Twitter>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.instagram.com/kikiyt90/" target="_blank" class="text-gray-900 hover:text-gray-900 dark:hover:text-white dark:text-gray-300">
-                                <Instagram></Instagram>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://open.spotify.com/user/kerstingirl90" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Spotify></Spotify>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="text-center text-gray-500 dark:text-gray-400">
-                    <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://cdn.discordapp.com/avatars/622784776234991626/1f8b78f17426329ad417ff3e5f2b0bc0.webp?size=128" alt="Bonnie Avatar">
-                    <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        <a href="#">MiauRizius</a>
-                    </h3>
-                    <p>Full-Stack Development / Management</p>
-                    <ul class="flex justify-center mt-4 space-x-4">
-                        <li>
-                            <a href="https://twitch.tv/miaurizius" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Twitch></Twitch>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/MiauRizius" target="_blank" class="text-gray-900 hover:text-gray-900 dark:hover:text-white dark:text-gray-300">
-                                <GitHub></GitHub>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/MiauRizius" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Twitter></Twitter>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.instagram.com/miaurizius/" target="_blank" class="text-gray-900 hover:text-gray-900 dark:hover:text-white dark:text-gray-300">
-                                <Instagram></Instagram>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://open.spotify.com/user/ep9eydfcne13pk3jsnmq52r8e" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Spotify></Spotify>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="text-center text-gray-500 dark:text-gray-400">
-                    <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://cdn.discordapp.com/avatars/797925384934260796/d0f62df7d914abf3b7f8c23ec3fda55b.webp?size=128" alt="Jese Avatar">
-                    <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        <a href="#">Hina</a>
-                    </h3>
-                    <p>Team Leitung</p>
-                    <ul class="flex justify-center mt-4 space-x-4">
-                        <li>
-                            <a href="https://open.spotify.com/user/xolgx30uoo85f68d6p6ecidjq" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Spotify></Spotify>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="text-center text-gray-500 dark:text-gray-400">
-                    <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://cdn.discordapp.com/avatars/818556798461149248/49bd0ccfc2a154b9d5f64112e1d64bc2.webp?size=128" alt="Joseph Avatar">
-                    <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        <a href="#">Kazuha</a>
-                    </h3>
-                    <p>Moderation</p>
-                    <ul class="flex justify-center mt-4 space-x-4">
-                        <li>
-                            <a href="https://twitter.com/einfach_kazuha" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Twitter></Twitter>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://open.spotify.com/user/le8e9a7zv701fr6pns7pg7oy6" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Spotify></Spotify>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="text-center text-gray-500 dark:text-gray-400">
-                    <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://cdn.discordapp.com/avatars/806677244431826974/76cdc95c6748102922e1ff8c00d3a5f1.webp?size=128" alt="Sofia Avatar">
-                    <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        <a href="#">Woody</a>
-                    </h3>
-                    <p>Moderation</p>
-                    <ul class="flex justify-center mt-4 space-x-4">
-                        <li>
-                            <a href="https://twitch.tv/woodyfs1980" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Twitch></Twitch>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/NoahS1304" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Twitter></Twitter>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.instagram.com/noah.s.offiziell/" target="_blank" class="text-gray-900 hover:text-gray-900 dark:hover:text-white dark:text-gray-300">
-                                <Instagram></Instagram>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://open.spotify.com/user/oltxfzahi9w9kj65wgutvepdh" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Spotify></Spotify>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="text-center text-gray-500 dark:text-gray-400">
-                    <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://cdn.discordapp.com/avatars/589161083554758682/aeb7c561cba3d1dd9568b4c255b979a8.webp?size=128" alt="Sofia Avatar">
-                    <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        <a href="#">NicyAmSleepen</a>
-                    </h3>
-                    <p>Moderation</p>
-                    <ul class="flex justify-center mt-4 space-x-4">
-                        <li>
-                            <a href="https://twitch.tv/nicyamsleepen" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Twitch></Twitch>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/NicyAmSleepen" target="_blank" class="text-gray-900 hover:text-gray-900 dark:hover:text-white dark:text-gray-300">
-                                <GitHub></GitHub>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.instagram.com/nicyamsleepn/" target="_blank" class="text-gray-900 hover:text-gray-900 dark:hover:text-white dark:text-gray-300">
-                                <Instagram></Instagram>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://open.spotify.com/user/nhi5gpof2w1axrpaqbaqqpyhb" target="_blank" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                                <Spotify></Spotify>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+
+                <TeamContainer 
+                name="KikiYT" 
+                rank="Creator / Inhaber" 
+                img="https://cdn.discordapp.com/avatars/234765355178917901/6706d060efe5caf93ebb770371a713ab.webp?size=128"
+                ttv="https://twitch.tv/kikiyt90"
+                github="https://github.com/KikiYT90"
+                twitter="https://twitter.com/KikiYT90"
+                insta="https://www.instagram.com/kikiyt90/"
+                spotify="https://open.spotify.com/user/kerstingirl90"></TeamContainer>
+
+                <TeamContainer 
+                name="MiauRizius" 
+                rank="Full-Stack Development / Management" 
+                img="https://cdn.discordapp.com/avatars/622784776234991626/1f8b78f17426329ad417ff3e5f2b0bc0.webp?size=128"
+                ttv="https://twitch.tv/miaurizius"
+                github="https://github.com/MiauRizius"
+                twitter="https://twitter.com/MiauRizius"
+                insta="https://www.instagram.com/miaurizius"
+                spotify="https://open.spotify.com/user/ep9eydfcne13pk3jsnmq52r8e"></TeamContainer>
+
+                <TeamContainer 
+                name="Hina" 
+                rank="Team Leitung" 
+                img="https://cdn.discordapp.com/avatars/797925384934260796/d0f62df7d914abf3b7f8c23ec3fda55b.webp?size=128"
+                spotify="https://open.spotify.com/user/xolgx30uoo85f68d6p6ecidjq"></TeamContainer>
+
+                <TeamContainer 
+                name="Woody" 
+                rank="Artist / Moderation" 
+                img="https://cdn.discordapp.com/avatars/806677244431826974/76cdc95c6748102922e1ff8c00d3a5f1.webp?size=128"
+                ttv="https://twitch.tv/woodyfs1980"
+                twitter="https://twitter.com/NoahS1304"
+                insta="https://www.instagram.com/noah.s.offiziell/"
+                spotify="https://open.spotify.com/user/oltxfzahi9w9kj65wgutvepdh"></TeamContainer>
+
+                <TeamContainer 
+                name="Kazuha" 
+                rank="Moderation" 
+                img="https://cdn.discordapp.com/avatars/818556798461149248/49bd0ccfc2a154b9d5f64112e1d64bc2.webp?size=128"
+                twitter="https://twitter.com/einfach_kazuha"
+                spotify="https://open.spotify.com/user/le8e9a7zv701fr6pns7pg7oy6"></TeamContainer>
+
+                <TeamContainer 
+                name="NicyAmSleepen" 
+                rank="Moderation" 
+                img="https://cdn.discordapp.com/avatars/589161083554758682/aeb7c561cba3d1dd9568b4c255b979a8.webp?size=128"
+                ttv="https://twitch.tv/nicyamsleepen"
+                github="https://github.com/NicyAmSleepen"
+                insta="https://www.instagram.com/nicyamsleepn/"
+                spotify="https://open.spotify.com/user/nhi5gpof2w1axrpaqbaqqpyhb"></TeamContainer>
+
             </div>  
         </div>
     </section>
